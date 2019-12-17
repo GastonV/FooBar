2bb27dd742a1ffc8c67909df6e236c964e3b48fd
Sirmais <atis.sirmais@gmail.com>
2019-12-17 13:33:07 +0100

    Push

---
+++ FooBar/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace FooBar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                if ((i % 3 ==0)&&(i%5==0))
+                {
+                    Console.WriteLine("FooBar");
+                }
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine("Foo");
+                }
+                if (i % 5 == 0)
+                {
+                    Console.WriteLine("Bar");
+                }
+            }
+        }
+
+    }
+}
